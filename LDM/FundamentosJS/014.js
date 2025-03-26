47ac6a1b0996c41a2c735127f08dc5cbcc690656
savamidev <migue.626@gmail.com>
2025-03-26 10:25:43 +0100

    Ejercicio 014 finalizado

---
+++ LDM/FundamentosJS/014.js
@@ -0,0 +1,9 @@
+"use strict"
+
+let a = +prompt("Introduce el número a:");
+let b = +prompt("Introduce el número b:");
+
+let result = a + b;
+
+
+alert((result < 4) ? "Baja la ropa sucia." : "Sube los zapatos");
