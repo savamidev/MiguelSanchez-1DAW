a4ffa5505138087d5369ba90964a1a796c964771
savamidev <migue.626@gmail.com>
2025-03-31 00:39:42 +0200

    Correción en la conversión de entradas a números para la suma

--- LDM/FundamentosJS/009.js
+++ LDM/FundamentosJS/009.js
@@ -1,8 +1,8 @@
 "use strict"
 
-let a = prompt("¿Primer número?", 1);
+let a = +prompt("¿Primer número?", 1);
 
-let b = prompt("¿Segundo número?", 2);
+let b = +prompt("¿Segundo número?", 2);
 
-alert((+a) + (+b));
+alert(a + b);
 
