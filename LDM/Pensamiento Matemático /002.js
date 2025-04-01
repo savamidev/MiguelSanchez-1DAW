1254279b547f6a21564e84c71c3c0fd54777f458
savamidev <migue.626@gmail.com>
2025-04-01 19:05:30 +0200

    Añadir lógica para calcular y mostrar la cantidad de monos y plátanos en 002.js

--- LDM/Pensamiento Matemático /002.js
+++ LDM/Pensamiento Matemático /002.js
@@ -1,1 +1,53 @@
 "use strict"
+
+// Use 🐵 to monkey and 🍌 to banana
+let monkeyBanana = ["🐵", "🍌", "🐵", "🍌", "🍌", "🍌", "🐵", "🍌", "🍌", "🍌", "🍌", "🍌", "🐵", "🍌", "🍌", "🍌", "🍌", "🍌", "🐵"];
+
+let monkey = calcNumMonkey();
+
+let banana = calcNumBanana();
+
+// Calculate the number of monkeys and bananas to eat
+let minBananasMonkey = Math.floor(banana / monkey);
+let restBananas = banana % monkey;
+
+showMessage();
+
+
+// Function to calculate the number of monkeys and bananas
+function calcNumMonkey() {
+    let numberOfMonkeys = 0;
+    monkeyBanana.forEach((item) => {
+        if (item === "🐵") {
+            numberOfMonkeys++;
+        }
+    }
+    );
+    return numberOfMonkeys;
+}
+
+
+function calcNumBanana() {
+    let numberOfBananas = 0;
+    monkeyBanana.forEach((item) => {
+        if (item === "🍌") {
+            numberOfBananas++;
+        }
+    }
+    );
+    return numberOfBananas;
+}
+
+function showMessage() {
+    let message = "";
+
+    for (let i = 0; i < monkey; i++) {
+        if (i < restBananas) {
+            message += `🐵${("🍌").repeat(restBananas + minBananasMonkey)}`
+        } else {
+            message += `🐵${("🍌").repeat(minBananasMonkey)}`
+        }
+    }
+    console.log(message);
+}
+
