61359893a121634fb92cf97138df9469bd57ba18
savamidev <migue.626@gmail.com>
2025-04-01 20:47:43 +0200

    Añadir lógica para determinar la comida según la preferencia vegetariana y la comida del día

--- LDM/Pensamiento Matemático /003.js
+++ LDM/Pensamiento Matemático /003.js
@@ -1,2 +1,10 @@
 "use strict"
 
+let foodUser = prompt("Are you going to eat lunch or dinner?");
+
+let isVegetarian = prompt("Are you vegetarian?") === "yes" ? true : false;
+
+console.log(
+    (foodUser === "lunch") ? (isVegetarian) ? "🥗" : "🍔" :
+    (isVegetarian) ? "🍝" : "🍕" 
+);
