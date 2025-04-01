ac9f6cf6b4f48d68a5c6fbe8d16c42b3a39d9a7a
savamidev <migue.626@gmail.com>
2025-04-01 18:35:41 +0200

    Añadir bucle para imprimir números del 1 al 100 con condiciones específicas

--- LDM/Pensamiento Matemático /001.js
+++ LDM/Pensamiento Matemático /001.js
@@ -1,3 +1,9 @@
 "use strict"
 
-
+for (let i = 1; i <= 100; i++) {
+    console.log(
+       (i % 3 === 0 && i % 5 === 0) ? "meloso🐻" :
+       (i % 3 === 0) ? "miel🍯" :
+       (i % 5 === 0) ? "oso🐻" :
+       i);
+   }
