a5a18d3ee43760e54d55f9f976bbc94bd37c7615
savamidev <migue.626@gmail.com>
2025-04-02 18:28:02 +0200

    Añadir lógica para validar números de tarjeta en 006.js

--- LDM/Pensamiento Matemático /006.js
+++ LDM/Pensamiento Matemático /006.js
@@ -1,2 +1,21 @@
 "use strict"
 
+let numCard = prompt("Enter your card number: (XXXX-XXXX-XXXX-XXXX)").replace(/-/g, "");
+let card = numCard.split("").map(Number);
+
+console.log(
+    (calcSumCard() % 10 == 0) ? "Valid card number" : "Invalid card number"
+);
+
+function calcSumCard() {
+    let sumNumCard = 0;
+    
+    for (let i = (card.length - 1); i > 0; i--) {
+        if (i % 2 == 0 && card[i] != 0) {
+            sumNumCard += ((card[i] * 2) > 9 ? (card[i] * 2) - 9 : card[i] * 2);
+        }
+    }
+    return sumNumCard;
+}
+
+
