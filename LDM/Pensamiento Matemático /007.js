dcb83ac6a27d45dbac9767ef40f906e6b58013ee
savamidev <migue.626@gmail.com>
2025-04-02 21:26:56 +0200

    Add HTML and JS files for exercise 007 with strict mode and card validation logic

---
+++ LDM/Pensamiento Matemático /007.js
@@ -0,0 +1,58 @@
+"use strict"
+
+let numCard = prompt("Enter your card number: (XXXX-XXXX-XXXX-XXXX)").replace(/\D/g, "");
+let card = numCard.split("").map(Number);
+
+console.log(
+    (calcLuhn() % 10 === 0) ? `Your type of card is ${calcCardType()} 💳 ✅` : "Invalid card ❌"
+);
+
+function calcLuhn() {
+    let sumNumCard = 0;
+    
+    for (let i = (card.length - 1); i >= 0; i--) {
+        let num;
+
+        if (i % 2 === 0) {
+            num = (card[i] * 2 < 9) ? (card[i] * 2) : ((card[i] * 2) - 9)
+        
+        } else {
+            num = card[i];
+        }
+        sumNumCard += num;
+    }
+    return sumNumCard;
+}
+
+function calcCardType() {
+
+    switch(true) {
+        case IsCardAExpr():
+            return "American Express";
+
+        case IsCardVisa():
+            return "Visa";
+
+        case IsCardMCard():
+            return "Master Card";
+
+        default:
+            return "Does not belong to any type of card."
+    }
+}
+
+
+function IsCardAExpr() {
+    let firstNum = Number(card.slice(0, 2).join(""));
+    return card.length === 15 && [34, 37].includes(firstNum);
+}
+
+function IsCardVisa() {
+    return (card.length === 13 || card.length === 16) && card[0] === 4;
+}
+
+function IsCardMCard() {
+    let firstNum = Number(card.slice(0, 2).join(""));
+    return card.length === 16 && [51, 52, 53, 54, 55].includes(firstNum);
+}
+
