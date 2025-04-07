0dbae7c2ce4e58838512be92a1415e9e6e46f09d
savamidev <migue.626@gmail.com>
2025-04-07 18:42:17 +0200

    Refactor game logic in 009.js to improve structure and enhance round-based gameplay functionality

--- LDM/Pensamiento Matemático /009.js
+++ LDM/Pensamiento Matemático /009.js
@@ -1,98 +1,88 @@
-"use strict"
+"use strict";
 
-const COLOR = ["red🔴","blue🔵", "green🟢", "yellow🟡"]
+const COLOR = ["red🔴", "blue🔵", "green🟢", "yellow🟡"];
 
 let userOption;
 let loop = 1;
 
-
-
 runGame();
 
-
 function runGame() {
+  let optionMenu = +prompt(
+    "Select game mode: \n1) InfityMode.\n2)Number roud to win. "
+  );
 
-    let optionMenu = +prompt("Select game mode: \n1) InfityMode.\n2)Number roud to win. ")
+  switch (optionMenu) {
+    case 1:
+      playGameInfity();
+      break;
 
-    switch (optionMenu) {
-        case 1:
-            playGameInfity();
-            break;
+    case 2:
+      playGameRound();
+      break;
+  }
 
-        case 2:
-            playGameRound();
-            break;    
-    }
-
-    alert("Thanks to play!")
+  alert("Thanks to play!");
 }
-
 
 function playGameRound() {
-    
-    let win = true;
-    let round = +prompt("How many rounds do you want to play?"); 
-    let roundPlay = 0;
-    
-    while (win == true && round > 0) {
+  let win = true;
+  let round = +prompt("How many rounds do you want to play?");
+  let roundPlay = 0;
 
-        let sequence = generateSequenceColor();
+  while (win == true && round > 0) {
+    let sequence = generateSequenceColor();
 
-        alert(sequence);
-        
-        userOption = prompt("Enter the color sequence:").toLowerCase();
+    alert(sequence);
 
-        win = (userOption == sequence) ? true : false;
+    userOption = prompt("Enter the color sequence:").toLowerCase();
 
-        showMessage(win, roundPlay);
+    win = userOption == sequence ? true : false;
 
-        round--;
-        roundPlay++;
-    }
+    showMessage(win, roundPlay);
+
+    round--;
+    roundPlay++;
+  }
 }
-
 
 function playGameInfity() {
-    
-    let win = true;
-    let round = 1; 
-    
-    while (userOption != "salir" && win == true) {
+  let win = true;
+  let round = 1;
 
-        let sequence = generateSequenceColor();
+  while (userOption != "salir" && win == true) {
+    let sequence = generateSequenceColor();
 
-        alert(sequence);
-        
-        userOption = prompt("Enter the color sequence:").toLowerCase();
+    alert(sequence);
 
-        win = (userOption == sequence) ? true : false;
+    userOption = prompt("Enter the color sequence:").toLowerCase();
 
-        showMessage(win, round);
+    win = userOption == sequence ? true : false;
 
-        round++;
-    }
+    showMessage(win, round);
+
+    round++;
+  }
 }
 
-
-function showMessage (result, round) {
-    (result == true) ? alert(`Your win, round ${round}`) : alert(`You lost, your have survived ${round}`)
+function showMessage(result, round) {
+  result == true
+    ? alert(`Your win, round ${round}`)
+    : alert(`You lost, your have survived ${round}`);
 }
-
 
 function generateSequenceColor() {
+  let message = "";
 
-    let message = "";
+  for (let i = 0; i < loop; i++) {
+    message += COLOR[selectIndexColor()];
+  }
 
-    for (let i = 0; i < loop; i++) {
-        message += COLOR[selectIndexColor()];
-    }
+  loop++;
 
-    loop++;
-
-    return message;
+  return message;
 }
 
-
 function selectIndexColor() {
-    return Math.floor(Math.random() * COLOR.length); 
+  return Math.floor(Math.random() * COLOR.length);
 }
