6fdcbbb1eddf55bfd347f00948b8beaa15a31de9
savamidev <migue.626@gmail.com>
2025-04-07 21:44:44 +0200

    Add initial HTML and JavaScript files for String-Array-Map functionality

---
+++ LDM/String-Array-Map/001.js
@@ -0,0 +1,12 @@
+"use strict"
+
+let thor = "Thor es el hijo de Odín";
+
+console.log(thor.length);
+console.log(thor.indexOf("o"));
+console.log(thor.charAt(3));
+console.log(thor.charAt(1));
+console.log(thor.split(" "));
+console.log(thor.replace("Thor", "Loki"));
+console.log(thor.slice(19, 23));
+console.log(thor.indexOf("a"));
