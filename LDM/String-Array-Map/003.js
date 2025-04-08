72440f12055c595c65dc6e1a78537f128e448ae3
savamidev <migue.626@gmail.com>
2025-04-08 11:06:55 +0200

    Add HTML and JavaScript files for Disney character manipulation functionality

---
+++ LDM/String-Array-Map/003.js
@@ -0,0 +1,43 @@
+"use strict"
+
+let disney = [
+
+    { nombre: "Hércules", pelicula: "Hércules" },
+ 
+    { nombre: "Megana", pelicula: "Hércules" },
+ 
+    { nombre: "Hades", pelicula: "Hércules" },
+ 
+    { nombre: "Campanilla", pelicula: "Peter Pan" },
+ 
+    { nombre: "Wendy", pelicula: "Peter Pan" }
+ 
+ ];
+ 
+disney.push({ nombre: "Peter Pan", pelicula: "Peter Pan" });
+console.log(disney);
+
+disney.unshift({ nombre: "Capitán Garfio", pelicula: "Peter Pan" });
+console.log(disney);
+
+disney.splice(1, 0, { nombre: "Cocodrilo", pelicula: "Peter Pan"});
+console.log(disney);
+
+disney.forEach((item) => {
+    console.log(item)
+});
+
+let indice = disney.findIndex(personaje => personaje.nombre === "Campanilla")
+console.log(`El índice de Campanilla es ${indice}`);
+
+let cocoFind = disney.find(personaje => personaje.nombre === "Cocodrilo");
+console.log(`Datos buscados: ${cocoFind.nombre}`);
+
+
+function shuffle() {
+   disney.sort();
+}
+
+shuffle();
+
+disney.forEach(personaje => console.log(personaje));
