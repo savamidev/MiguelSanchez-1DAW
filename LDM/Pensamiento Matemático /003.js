acf530f76c846bfd46509669ab1053b2b79c82d6
savamidev <migue.626@gmail.com>
2025-04-09 17:15:19 +0200

    Add food options to console output based on user input

--- LDM/Pensamiento Matemático /003.js
+++ LDM/Pensamiento Matemático /003.js
@@ -4,7 +4,8 @@ let foodUser = prompt("Are you going to eat lunch or dinner?");
 
 let isVegetarian = prompt("Are you vegetarian?") === "yes" ? true : false;
 
+// Imprimimos el mensaje según las respuestas del usuario
 console.log(
-    (foodUser === "lunch") ? (isVegetarian) ? "🥗" : "🍔" :
-    (isVegetarian) ? "🍝" : "🍕" 
+    (foodUser === "lunch") ? (isVegetarian) ? "Salad 🥗" : "Burger 🍔" :
+    (isVegetarian) ? "Italian paste 🍝" : "Pizza 🍕" 
 );
