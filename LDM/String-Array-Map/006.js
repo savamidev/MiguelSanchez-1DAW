c591819801a142562b22a29d5d577e5e45ed3b2d
savamidev <migue.626@gmail.com>
2025-04-09 18:43:57 +0200

    Enhance code readability by adding comments to explain the random number generation and value addition in the arraySet

--- LDM/String-Array-Map/006.js
+++ LDM/String-Array-Map/006.js
@@ -7,7 +7,10 @@ addValueArray();
 
 console.log(arraySet)
 
-
+/**
+ * Añadimos un número random ( generateRandom()), comprobando el tamaño del 
+ * arraySet para verificar que hemos agregado un valor no repetido
+ */
 function addValueArray() {
     while (arraySet.size < 7) {
         arraySet.add(generateRandom());        
@@ -15,7 +18,7 @@ function addValueArray() {
 };
 
 
-
+// Generamos un número random del 0 al 9
 function generateRandom() {
-    return Math.floor(Math.random() * 9);
+    return Math.floor(Math.random() * 10);
 };
