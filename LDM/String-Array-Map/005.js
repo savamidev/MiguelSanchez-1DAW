ab45adf6cbaac38c3248df68184e24dc9040c62e
savamidev <migue.626@gmail.com>
2025-04-09 18:40:17 +0200

    Fix formatting of comments for improved readability in grupo operations

--- LDM/String-Array-Map/005.js
+++ LDM/String-Array-Map/005.js
@@ -13,7 +13,7 @@ let grupos = [
  ];
  
 
- //filtra los grupos por género obtenido aquellos que son de Heavy Metal.
+ // filtra los grupos por género obtenido aquellos que son de Heavy Metal.
 grupos.forEach(item => {
     if (item.genero === "Heavy Metal") { 
         console.log(item);
@@ -21,12 +21,12 @@ grupos.forEach(item => {
 });
 
 
-//Busca en los grupos usando “find” el grupo con Nombre “Cold Play”.
+// Busca en los grupos usando “find” el grupo con Nombre “Cold Play”.
 console.log(
     grupos.find(grupo => (grupo.nombre === "Cold Play"))
 );
 
-//¿En qué posición del array se encuenta “Cold Play”?
+// ¿En qué posición del array se encuenta “Cold Play”?
 console.log(
     grupos.findIndex(grupo => grupo.nombre === "Cold Play")
 );
