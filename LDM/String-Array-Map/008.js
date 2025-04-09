1edf841699d86dc2db8b5c07bd70ae6a1e37faef
savamidev <migue.626@gmail.com>
2025-04-09 20:00:35 +0200

    Refactor 008.js to enhance object declaration and method usage for 'perro'

--- LDM/String-Array-Map/008.js
+++ LDM/String-Array-Map/008.js
@@ -1,2 +1,31 @@
-"use strict"
+"use strict";
 
+// Declaración de perro como objeto
+const perro = new Object();
+
+// Mostramos al objeto perro por consola
+console.log(perro);
+
+// Asigamos variables y valores al objeto perro
+perro.name = "Bora";
+perro.legs = 4;
+perro.color = "Blanco y marrón";
+perro.age = 13;
+perro.bark = function () {
+  console.log("Woof ".repeat(2));
+};
+
+// Llamada al método bark()
+perro.bark();
+
+
+// Imprimimos valores de interés del objeto perro
+console.log(`Name: ${perro.name}\nColor: ${perro.color}\nAge: ${perro.age}\nPerro lara: ${perro.bark()}`);
+
+// Establecer nuevas propiedades
+perro.breed = "Braco Alemán"
+perro.getInfor = function () {
+    console.log(`Name: ${perro.name}\nNumero de patas: ${perro.legs}\nColor: ${perro.color}\nAge: ${perro.age}\nRaza: ${perro.breed}`);
+};
+
+perro.getInfor();
