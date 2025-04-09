8d047bff2389f3000d9e90963f35ead2888e7ed3
savamidev <migue.626@gmail.com>
2025-04-09 18:34:53 +0200

    Enhance code readability by adding comments to fruit array operations

--- LDM/String-Array-Map/002.js
+++ LDM/String-Array-Map/002.js
@@ -2,20 +2,26 @@
 
 let fruits = ["Banana", "Manzana", "Fresa"];
 
+//Añadimos cereza al principio
 fruits.unshift("Cereza");
 console.log(fruits);
 
+//Añadimos melocotón al final
 fruits.push("Melocotón");
 console.log(fruits);
 
+//Mostrar el array elemento a elemento con forEach
 fruits.forEach((item) => console.log(item));
 
+//Eliminamos el primer elemento.
 fruits.shift();
 console.log(fruits);
 
+//Eliminamos el último elemento.
 fruits.pop();
 console.log(fruits);
 
+//Mostrar cada elemento con for of o for in, lo que considere más adecuado.
 for (let fruit of fruits) {
     console.log(fruit)
 };
