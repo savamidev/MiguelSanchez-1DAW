57beaefa9b1baf94525ee0d89272baf5296edfd4
savamidev <migue.626@gmail.com>
2025-04-09 18:39:17 +0200

    Enhance code readability by adding comments to explain the use of .map() for extracting band names

--- LDM/String-Array-Map/004.js
+++ LDM/String-Array-Map/004.js
@@ -8,6 +8,10 @@ let guns= { name: "Guns and Roses", age: 28 };
 
 let bands = [ acdc, metallica, guns];
 
+/* 
+* .map() nos permite crear un nuevo array según ciertas condiciones
+* en este caso con los nombres de cada banda
+*/
 let names = bands.map(bands => bands.name);
     
 console.log(names);
