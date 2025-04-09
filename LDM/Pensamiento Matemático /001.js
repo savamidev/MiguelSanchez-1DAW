de5bca7bfbc7cd57a51e4ad7f3481eb897ed12ee
savamidev <migue.626@gmail.com>
2025-04-09 17:02:47 +0200

    Add comment to explain loop functionality in 001.js

--- LDM/Pensamiento Matemático /001.js
+++ LDM/Pensamiento Matemático /001.js
@@ -1,5 +1,6 @@
 "use strict"
 
+// Uso de bucle para iterar del 1 al 100, comprobando el resultado de la impresión.
 for (let i = 1; i <= 100; i++) {
     console.log(
        (i % 3 === 0 && i % 5 === 0) ? "meloso🐻" :
