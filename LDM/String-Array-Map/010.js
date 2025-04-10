e9e61dbea6c0edc349272e60a681e02b093c5cce
savamidev <migue.626@gmail.com>
2025-04-10 19:20:54 +0200

    Add Map object and key-value pairs for character descriptions in project 010

--- LDM/String-Array-Map/010.js
+++ LDM/String-Array-Map/010.js
@@ -1,2 +1,22 @@
 "use strict"
 
+// Creamos nuestro objeto Map
+let miMapa = new Map();
+
+// Añadimos clave-valor al mapa
+miMapa.set("Carl Fredricksen", "Protagonista principal");
+miMapa.set("Russell", "Explorador entusiasta");
+miMapa.set("Dug", "Perro que habla");
+miMapa.set("Kevin", "Ave tropical gigante");
+miMapa.set("Charles Muntz", "Antagonista");
+
+// Mostramos el mapa
+console.log(miMapa);
+
+// Comprobamos si existen "Russell" y "Alpha" con has()
+let busqueda = (map, item) => map.has(item);
+
+console.log(busqueda(miMapa, "Russell"));
+console.log(busqueda(miMapa, "Alpha"));
+
+// 
