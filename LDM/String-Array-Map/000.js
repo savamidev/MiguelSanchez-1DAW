246c0027fa85a86666b1deba4f210d8f20157d46
savamidev <migue.626@gmail.com>
2025-04-10 12:18:47 +0200

    Add initial HTML and JavaScript files for project 000 setup

---
+++ LDM/String-Array-Map/000.js
@@ -0,0 +1,37 @@
+"use srtict"
+
+// Declara un array vacío.
+let arrayEmpty = [];
+
+// Declara un array com mas de 5 elementos.
+let arrayElements = [1, 2, 3, 4, 5, 6, 7];
+
+// Encuentra la longitud de tu array.
+let showLongitudArray = function (array ) {
+    console.log(array.length);
+};
+
+showLongitudArray(arrayElements);
+
+// Obtenga el primer elemento, el elemento del medio y el último elemento de un array.
+
+
+// Declara un array llamado mixedDataTypes, coloque diferentes tipos de datos en el array y encuentre la longitud del array. El tamaño del array debe ser mayor que 5.
+// Declare un variable array de nombre itCompanies y asignarles valores iniciales Facebook, Google, Microsoft, Apple, IBM, Oracle y Amazon.
+// Imprima el array usando console.log().
+// Imprima el número de empresas en el array.
+// Imprime la primer empresa , la intermedia y la última empresa
+// Imprime cada empresa.
+// Cambie el nombre de cada empresa a mayúsculas uno por uno e imprímalos.
+// Imprime el array como una oración: Facebook, Google, Microsoft, Apple, IBM, Oracle y Amazon son grandes empresas de TI.
+// Compruebe si existe una determinada empresa en el array itCompanies. Si existe, retorna la empresa; de lo contrario, retorna la empresa no existe
+// Filtre las empresas que tienen más de una 'o' sin el método filter()
+// Ordene el array usando el método sort()
+// Invierte la array usando el método reverse()
+// Cortar las primeras 3 empresas del array
+// Cortar las últimas 3 empresas del array
+// Cortar la empresa o empresas intermedias de TI del array
+// Eliminar la primera empresa de TI del array
+// Eliminar la empresa o empresas intermedias de TI del array
+// Elimine la última empresa de TI del array
+// Eliminar todas las empresas de TI
