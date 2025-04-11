b8faedf77e2d6357be288941ecdeb99ed5284bec
savamidev <migue.626@gmail.com>
2025-04-11 18:13:20 +0200

    Enhance Map operations by adding character role retrieval, updating values, deleting entries, and iterating through descriptions

--- LDM/String-Array-Map/010.js
+++ LDM/String-Array-Map/010.js
@@ -19,4 +19,25 @@ let busqueda = (map, item) => map.has(item);
 console.log(busqueda(miMapa, "Russell"));
 console.log(busqueda(miMapa, "Alpha"));
 
-// 
+// Obtén y muestra el rol del personaje "Dug" con get().
+console.log(miMapa.get("Dug"));
+
+//Actualiza el valor asociado a "Russell" a "Explorador leal y valiente" y muestra el nuevo valor.
+miMapa.set("Russell", "Explorador leal y valiente");
+console.log(miMapa);
+
+//Elimina al personaje "Charles Muntz" del Map usando delete() y muestra el contenido restante.
+miMapa.delete("Charles Muntz");
+console.log(miMapa);
+
+//Itera sobre los personajes y sus descripciones con forEach() y muestra cada par formateado así:
+miMapa.forEach((clave, valor) => 
+    console.log(`"${clave}: ${valor}"`)
+);
+
+//Muestra cuántos personajes hay actualmente en el Map utilizando size.
+console.log(miMapa.size);
+
+//Vacía el Map usando clear() y verifica que esté vacío mostrando su contenido y tamaño.
+miMapa.clear();
+console.log(miMapa);
