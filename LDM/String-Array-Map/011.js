a5dc949735adb78c9d7de439469277c6f2efa8fe
savamidev <migue.626@gmail.com>
2025-04-12 12:08:51 +0200

    Refactor CursoOnline class to improve student management methods and enhance functionality for adding, removing, and binding students.

--- LDM/String-Array-Map/011.js
+++ LDM/String-Array-Map/011.js
@@ -1,6 +1,6 @@
 "use strict";
 
-class GestorCurso {
+class CursoOnline {
   constructor(nombre, especialidad, duracion) {
     this.nombre = nombre;
     this.especialidad = especialidad;
@@ -34,21 +34,23 @@ class GestorCurso {
   }
 
   /**
- * Elimina un estudiante de la lista de estudiantes del curso.
- * Solicita al usuario el nombre y apellidos del estudiante a eliminar.
- * Si el estudiante existe, se elimina de la base de datos; de lo contrario, se muestra un mensaje de error.
- * 
- * @returns {void} No retorna ningún valor.
- */
+   * Elimina un estudiante de la lista de estudiantes del curso.
+   * Solicita al usuario el nombre y apellidos del estudiante a eliminar.
+   * Si el estudiante existe, se elimina de la base de datos; de lo contrario, se muestra un mensaje de error.
+   *
+   * @returns {void} No retorna ningún valor.
+   */
   removeEstudiante() {
     let nombre = prompt("Introduce el nombre del estudiante a eliminar:");
-    let apellidos = prompt("Introduce los apellidos del estudiante a eliminar:");
+    let apellidos = prompt(
+      "Introduce los apellidos del estudiante a eliminar:"
+    );
 
     const estudianteDelete = { nombre, apellidos };
 
     if (this.isExistsEstudiante(estudianteDelete)) {
       this.estudiantes.delete(estudianteDelete);
-      
+
       console.log(
         `El estudiante ${estudianteDelete.nombre} ${estudianteDelete.apellidos} ha sido eliminado correctamente.`
       );
@@ -59,9 +61,41 @@ class GestorCurso {
     }
   }
 
-  //-------------
+  /**
+   * Une los estudiantes de este curso con los estudiantes de otro curso.
+   *
+   * Este método recibe un objeto de curso como parámetro y crea una unión de
+   * estudiantes únicos utilizando un conjunto Set. Los estudiantes duplicados
+   * se eliminan automáticamente, asegurando que cada estudiante aparezca solo
+   * una vez en el resultado.
+   *
+   * @param {object} otroCurso - El otro curso del cual se unirán los estudiantes.
+   * @returns {Set} Un conjunto (Set) de estudiantes que incluye todos los
+   *                estudiantes de ambos cursos.
+   */
+  bindEstudiantes(otroCurso) {
+    let bindListSet = new Set([...this.estudiantes, ...otroCurso.estudiantes]);
 
-  // Métodos generales
+    return bindListSet;
+  }
+
+  /**
+   * Devulve la lista de estudiantes comunes entre este curso y otro curso.
+   *
+   * Este método compara los estudiantes de este curso con los estudiantes
+   * de otro curso y devuelve un conjunto Set que contiene solo aquellos
+   * estudiantes que están presentes en ambos cursos.
+   *
+   * @param {object} otroCurso - El otro curso del cual se compararán los estudiantes.
+   * @returns {Set} Un conjunto Set de estudiantes que están presentes en ambos cursos.
+   */
+  commonEstudiantes(otroCurso) {
+    let commonListSet = new Set(
+      [...this.estudiantes].filter((item) => otroCurso.estudiantes.has(item))
+    );
+
+    return commonListSet;
+  }
 
   /**
    * Comprueba si un estudiante está en el curso.
@@ -71,4 +105,21 @@ class GestorCurso {
   isExistsEstudiante(estudiante) {
     return this.estudiantes.has(estudiante);
   }
+
+  /**
+   * Vacía el conjunto de estudiantes del curso.
+   */
+  vaciarEstudiantes() {
+    let confirmation = prompt(
+      "Por seguridad, desea eliminar la lista de estudiantes? (Si o no)",
+      no
+    );
+
+    if (confirmation.toLowerCase === si) {
+      this.estudiantes.clear();
+      console.log(`El curso "${this.nombre}" ha sido vaciado.`);
+    }
+  }
+
+  
 }
